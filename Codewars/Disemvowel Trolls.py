c0aa730c5a1ad06dd51516b7292c0c314f92fda8
patricia <patriciaperezcruz007@gmail.com>
2025-03-18 13:53:22 -0600

    Codedex =)

---
+++ Codewars/Disemvowel Trolls.py
@@ -0,0 +1,25 @@
+"""
+Nombre: Patricia Pérez Cruz
+Descripción:
+Task 7 kyu
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+"""
+
+def disemvowel(string_):
+    vocal = "aeiou"
+    result = ""
+    for i in string_:
+        if i.lower() not in vocal:
+            result += i
+    return result
+
+cadena = input("Ingrese su cadena a usar: ")
+print(f"La cadena corregida es: {disemvowel(cadena)}")
---
+++ Codewars/Flick Switch.py
@@ -0,0 +1,33 @@
+"""
+Nombre: Patricia Pérez Cruz
+Descripción:
+Task 8 kyu
+Create a function that always returns True/true for every item in a given list.
+However, if an element is the word 'flick', switch to always returning the opposite boolean value.
+
+Examples
+['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+
+['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+
+['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+"""
+
+
+
+def flick_switch(jsjs):
+    state = True
+    result = []
+
+    for item in jsjs:
+        if item == 'flick':
+            state = not state
+        result.append(state)
+
+    return result
+
+
+#mis pruebas
+print(flick_switch(['codewars', 'flick', 'code', 'wars']))
+print(flick_switch(['flick', 'chocolate', 'adventure', 'sunshine']))
+print(flick_switch(['bicycle', 'jarmony', 'flick', 'sheep', 'flick']))

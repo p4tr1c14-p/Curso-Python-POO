390ba8ffaff3f7ebc43a5d8c4b67d5069c166e68
Patricia Pérez Cruz <163355108+p4tr1c14-p@users.noreply.github.com>
2025-03-19 15:56:56 -0600

    Subí 19/03/2025

---
+++ Clases-y-Objetos/Relaciones_entre_objetos.py
@@ -0,0 +1,62 @@
+class Empleado:
+    no_id = 1
+    def __init__(self, nombre : str, sueldo: float):
+        self.nombre = nombre
+        self.sueldo = sueldo
+
+        self.no_id = Empleado.no_id
+        Empleado.no_id += 1
+
+    def aumentar_sueldo(self, porcentaje: float) -> None:
+        """
+        Se utiliza para aumentar el sueldo de acuerdo con un porcentaje.
+        :param porcentaje: Porcentaje a incrementar el sueldo.
+        """
+        if porcentaje > 0:
+            self.sueldo += self.sueldo * (porcentaje / 100)
+            print(f"El sueldo de {self.nombre} ahora es de {self.sueldo:,.2f}")
+
+        else:
+            print("No se aplicó ningún cambio, ya que por Ley, el sueldo no puede disminuir.")
+
+    def __str__(self) -> str:
+        return f"Empleado(id: {self.no_id}, Nombre: {self.nombre}, Sueldo: {self.sueldo:,.2f})"
+
+class Empresa:
+    def __init__(self, nombre : str, *empleados: Empleado):
+        self.nombre = nombre
+        self.empleados = list(empleados)
+
+    def agregar_empleados(self, *nuevos_empleados: Empleado):
+        for empleado in list(nuevos_empleados):
+            self.empleados.append(empleado)
+
+    def mostrar_empleados(self) -> None:
+        """
+        Se utiliza para mostrar los empleados de la empresa en forma de lista.
+        """
+        print(f"*** Lista de empleados de {self.nombre} ***")
+
+        for empleado in self.empleados:
+            print(empleado)
+
+    def __str__(self) -> str:
+        """
+        Se utiliza para mostrar los empleados de la empresa en forma de lista.
+        """
+        # Se convierte los elementos de la lista en cadenas (invocando str() para cada uno de ellos) y
+        # se unen con ", " a través del metodo str.join().
+        # Este patrón es muy común en Python para obtener una cadena a partir de una lista.
+        empleados = ", ".join(str(empleado) for empleado in self.empleados)
+        return f"Empresa({self.nombre = }, {empleados})"
+
+if __name__ == '__main__':
+    sofia = Empleado("Sofi", 120)
+    lusi = Empleado("Lusi", 1000)
+
+    unsij = Empresa("UNSIJ")
+    unsij.agregar_empleados(lusi, sofia)
+
+    print(unsij)
+    print(sofia)
+    print(lusi)
